51ee62547dc439f1d0292438a15c633bf9a7eaa4
Gomtesh Patil <89897854+GomteshPatil@users.noreply.github.com>
2021-09-02 19:32:19 +0530

    Add files via upload

---
+++ Arithopp.java
@@ -0,0 +1,19 @@
+class Arithopp{
+
+	public static void main(String orgs[]){
+	
+	int num1=20;
+	int num2=15;
+	
+	int add=num1+num2;
+	int sub=num1-num2;
+	int mult=num1*num2;
+	double div=num1/num2;
+	System.out.println("The two numbers that are given for operation are 20 and 15");
+	System.out.println("The value of addition is:"+add);
+	System.out.println("The value of substraction is :"+sub);
+	System.out.println("The value of multiplication is :"+mult);
+	System.out.println("The value of devision is :"+div);
+	
+	}
+}
