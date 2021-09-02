ee1aea6e3ed60efb22bb6ebd9e8421ac0ee721a5
Gomtesh Patil <89897854+GomteshPatil@users.noreply.github.com>
2021-09-02 19:28:47 +0530

    Add files via upload

---
+++ Addthedubble.java
@@ -0,0 +1,15 @@
+class Addthedubble{
+
+	public static void main(String orgs[]){
+	
+	double numX;
+	numX=23.121999;
+	double numY;
+	numY=31.012000;
+	double Result=numX+numY;	
+	System.out.println("the sum of numX and numY is:"+Result);
+
+	
+	}
+
+}
