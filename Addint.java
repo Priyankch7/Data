eaaa8503f4677da03f92ecf1f051c632615b57d7
Gomtesh Patil <89897854+GomteshPatil@users.noreply.github.com>
2021-09-02 19:07:24 +0530

    Add files via upload

---
+++ Addint.java
@@ -0,0 +1,15 @@
+class Addint{
+	
+	public static void main(String orgs[]){
+	
+	int num1;
+	num1=31;
+	int num2;
+	num2=23;
+	int Sum=num1+num2;
+	System.out.println("the sum of 23 and 31 is:"+Sum);
+
+	
+	}
+
+}
